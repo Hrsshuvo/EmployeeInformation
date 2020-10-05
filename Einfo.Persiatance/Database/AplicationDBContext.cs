5ab86b6d48d991bd38e17625db77a617f558b58e
shuvo <shuvo@DESKTOP-HOO3536>
2020-10-06 01:35:01 +0600

    Create Database file Create Db Context add entity frame work & add Connection string

---
+++ Einfo.Persiatance/Database/AplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using EInfo.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Einfo.Persiatance.Database
+{
+    public class AplicationDBContext : DbContext
+    {
+        public DbSet<Deparement> Deparements { get; set; }
+    }
+}
