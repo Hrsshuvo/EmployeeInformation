98efe3c500c2b06492c53a432e53d7f2ffa00865
shuvo <shuvo@DESKTOP-HOO3536>
2020-10-06 00:52:07 +0600

    Create Department Model & VM

---
+++ EInfo.Core/ViewModel/DeparemeentVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EInfo.Core.ViewModel
+{
+    public class DeparemeentVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
