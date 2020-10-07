f0d14132116ef0277665237abc6e7fb1fdb8efe4
shuvo <shuvo@DESKTOP-HOO3536>
2020-10-07 15:30:53 +0600

    Create Department form

---
+++ EmployeeInfoManagement/Controllers/DepartmentsController.cs
@@ -0,0 +1,19 @@
+﻿using EInfo.Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeInfoManagement.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        // GET: Departments
+        public ActionResult New()
+        {
+            var vm = new DeparemeentVM() { IsActive = true };
+            return View(vm);
+        }
+    }
+}
