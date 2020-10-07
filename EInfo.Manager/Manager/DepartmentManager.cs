f360c40bbbac0dbe028e81aea30a273b6da941b7
shuvo <shuvo@DESKTOP-HOO3536>
2020-10-07 12:49:53 +0600

    Depertment manager

--- EInfo.Manager/Manager/DepartmentManager.cs
+++ EInfo.Manager/Manager/DepartmentManager.cs
@@ -25,5 +25,31 @@ namespace EInfo.Manager.Manager
             return (Mapper.Map<Deparement, DeparemeentVM>(drpertments));
 
         }
+        public IEnumerable<DeparemeentVM> GetAll()
+        {
+            var drpertments = _aplicationDBContext.Deparements.ToList().Select(Mapper.Map<Deparement, DeparemeentVM>);
+            return drpertments;
+        }
+        public int Add(DeparemeentVM vm)
+        {
+            var etity = Mapper.Map<DeparemeentVM, Deparement>(vm);
+            _aplicationDBContext.Deparements.Add(etity);
+            var isSave = _aplicationDBContext.SaveChanges();
+            return isSave;
+        }
+        public int Update(int id,DeparemeentVM vm)
+        {
+            var entity = _aplicationDBContext.Deparements.SingleOrDefault(c => c.Id == id);
+            Mapper.Map(vm,entity);
+            var isUpdate = _aplicationDBContext.SaveChanges();
+            return isUpdate;
+        }
+        public int Remove(int id)
+        {
+            var entity = _aplicationDBContext.Deparements.SingleOrDefault(c => c.Id == id);
+            _aplicationDBContext.Deparements.Remove(entity);
+            var isRemove = _aplicationDBContext.SaveChanges();
+            return isRemove;
+        }
     }
 }
